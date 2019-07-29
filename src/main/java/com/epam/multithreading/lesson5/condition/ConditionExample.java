377b9bd3e5445f8332f7c9254841a3ef563bf477
Sergey Kuznetsov <azon.sk@gmail.com>
2019-07-29 15:07:19 +0300

    Update ConditionExample.java

--- src/main/java/com/epam/multithreading/lesson5/condition/ConditionExample.java
+++ src/main/java/com/epam/multithreading/lesson5/condition/ConditionExample.java
@@ -3,6 +3,42 @@ package com.epam.multithreading.lesson5.condition;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/*Применение условий в блокировках позволяет добиться контроля над управлением доступом к потокам. Условие блокировки
+представлет собой объект интерфейса Condition из пакета java.util.concurrent.locks.
+
+Применение объектов Condition во многом аналогично использованию методов wait/notify/notifyAll класса Object, 
+которые были рассмотрены в одной из прошлых тем. В частности, мы можем использовать следующие методы интерфейса Condition:
+
+    await: поток ожидает, пока не будет выполнено некоторое условие и пока другой поток не вызовет методы signal/signalAll.
+        Во многом аналогичен методу wait класса Object
+
+    signal: сигнализирует, что поток, у которого ранее был вызван метод await(), может продолжить работу. Применение
+    аналогично использованию методу notify класса Object
+
+    signalAll: сигнализирует всем потокам, у которых ранее был вызван метод await(), что они могут продолжить работу. 
+        Аналогичен методу notifyAll() класса Object
+
+Эти методы вызываются из блока кода, который попадает под действие блокировки ReentrantLock. Сначала, используя эту 
+блокировку, нам надо получить объект Condition:
+
+ReentrantLock locker = new ReentrantLock();
+Condition condition = locker.newCondition();
+
+Как правило, сначала проверяется условие доступа. Если соблюдается условие, то поток ожидает, пока условие не изменится:
+1
+2
+	
+while (условие)
+    condition.await();
+
+После выполнения всех действий другим потокам подается сигнал об изменении условия:
+1
+	
+condition.signalAll();
+
+Важно в конце вызвать метод signal/signalAll, чтобы избежать возможности взаимоблокировки потоков.
+*/
+
 public class ConditionExample {
     public static void main(String[] args) {
         Store store = new Store();
@@ -12,7 +48,6 @@ public class ConditionExample {
         new Thread(consumer).start();
     }
 }
-
 
 class Store {
     private ReentrantLock lock;
